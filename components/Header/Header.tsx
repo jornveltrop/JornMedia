9786dfa6e195382a5101fc4a93aadc116e02279f
jornveltrop <jorn1veltrop@gmail.com>
2022-09-22 11:39:34 +0200

    Home: Adding extra's like toTop

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Navigation from '../Navigation/Navigation'
-import headerStyle from "./header.module.css"
+import headerStyle from './header.module.css'
 
 const Header = ({...props}) => {
   const headerData = props.headerData
