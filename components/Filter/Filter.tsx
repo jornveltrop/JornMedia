d95e9666248a7d304a48adbfee8551e87f7b08aa
jornveltrop <jorn1veltrop@gmail.com>
2022-09-23 18:28:23 +0200

    Filter: Added set-up for filter

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -4,9 +4,55 @@ import { useRouter } from 'next/router'
 
 const Filter = ({...props}) => {
     const skills = props.skills
+    const router = useRouter();
+    let selectedSkills: any[] = []
+    console.log(selectedSkills)
     
     function setQuery (item: any, value: boolean){
       console.log(item, value)
+      let currentQueries:any = router.query.skills
+      let newQueries
+
+      if (value == false) {
+        
+        if (currentQueries != undefined) {
+          if(currentQueries.indexOf(item) !== -1) {  
+            return
+          } else {  
+            newQueries = [currentQueries, item]
+          }
+        } else newQueries = [item]
+
+      } else if (value == true) {
+        console.log(currentQueries + "test")
+        let currentArray = 
+        currentQueries.splice(currentQueries.indexOf(item), 1);
+        newQueries = currentQueries
+
+        router.replace({
+          query:  {
+            skills: newQueries // override the color property
+          },
+        },
+        undefined,
+        {
+          shallow: false,
+        },
+      );
+      }
+
+      
+      router.replace({
+        query:  {
+          skills: newQueries[0] // override the color property
+        },
+      },
+      undefined,
+      {
+        shallow: false,
+      },
+    );
+
     }
 
   return (
